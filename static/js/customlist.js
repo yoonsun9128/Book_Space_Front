22eea169002af58aba7f503273b2e7f3c3651ab1
nhkmi1001 <nhkmi1001@gmail.com>
2022-12-13 22:34:29 +0900

    [MOD] 팀원들 기능 합침

--- static/js/customlist.js
+++ static/js/customlist.js
@@ -5,11 +5,11 @@ const frontend_base_url = 'http://127.0.0.1:5500/templates/'
 function show_customlist(){
     $.ajax({
         type: 'GET',
-        url:`${backend_base_url}articles/`,
+        url:`${backend_base_url}articles/recommend`,
         data: {},
         success: function(response) {
             let books = response
-            for (let i=0; i < 10; i++){
+            for (let i=0; i < books.length; i++){
                 append_temp_html(
                     books[i].id,
                     books[i].img_url,
@@ -26,40 +26,47 @@ function show_customlist(){
                 <div class="book-content">
                     <div class="book-title">${book_title}</div>
                     <div class="book-sum">${book_content}</div>
-                    <section title=".squaredFour">
-                        <div class="squaredFour" id=${id} onclick=save_id(this.id)>
-                            <input type="checkbox" value="None" id="squaredFour" name="check" checked />
-                            <label for="squaredFour"></label>
-                        </div>
-                    </section>
+                   
+                </div>
+                <div class="checkbox">
+                    <input class="check" type="checkbox" id="${id}"  name="check" onclick=save_id(this.id)>
                 </div>
                 `
                 $('#customlist').append(temp_html)
-                
-            
             }
-          }
         }
         
-
+    }
     )}show_customlist()
-let id_list = []
-function save_id(id){
-    id_list.push(id)
-    console.log(id_list)
+
+let select_books = [];
+function save_id(id){    
+    if($(`input:checkbox[id=${id}]`).is(":checked") == true){
+        select_books.push(id)
+    }
+    else if($(`input:checkbox[id=${id}]`).is(":checked") == false){
+        for(let i=1; i< select_books.length; i++){
+            if(select_books[i] == id){
+                select_books.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    console.log(select_books)
 }
 
+
 async function send_id(){
-    const response = await fetch(`${backend_base_url}articles/book/`, {
-            method: 'POST',
+    console.log(select_books)
+    const response = await fetch(`${backend_base_url}articles/user/?select_books=${select_books}`, {
+            method: 'GET',
             header: {
-                Accept: "application/json",
                 'Content-type':'application/json'
             },
-            body: JSON.stringify({"id_list":id_list}),
         })
         .then(res=> res.json())
         .then(data=> console.log(data));
-
-        response_json = await response.json()
+        
+        alert("메인페이지로 이동합니다")
+        window.location.href="../templates/main.html"
     }
