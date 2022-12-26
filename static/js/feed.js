e1777e3010740982f60ddc8e70f97d44ebd9f88a
cjstmdgusqw <cjstmdgusqw@gmail.com>
2022-12-26 22:08:29 +0900

    [MOD] : feed.html, feed.js

--- static/js/feed.js
+++ static/js/feed.js
@@ -4,9 +4,12 @@ const payload = localStorage.getItem('payload')
 const personObj = JSON.parse(payload)
 const userId = personObj['user_id']
 
+window.onload = function init(){
+    document.getElementById("best").value = "시간순"
+}
+
 function Best(){
     var A = document.getElementById("best").value;
-
 
 async function FeedList(){
     $('#image-box').empty()
@@ -22,6 +25,7 @@ async function FeedList(){
     }
     FeedData().then((data) => {
         feed = data
+        console.log(feed)
         for (let i = 0; i < feed.length; i++){
             let username = feed[i]['username']
             let title = feed[i]['title']
@@ -32,6 +36,7 @@ async function FeedList(){
             let count = feed[i]['likes_count']
             let profile_img = feed[i]['profile_img']
             let write_user_id = feed[i]['user_id']
+            let count_comment = feed[i]['comment_set'].length
 
             if (like.includes(userId) == true){
                 color = "red"
@@ -39,6 +44,7 @@ async function FeedList(){
             else{
                 color = "gray"
             }
+            
 
             let temp_html = `
             <div id = "my_table" class="col-md-4">
@@ -49,12 +55,13 @@ async function FeedList(){
                     <div class="card-img-top image-card image-card-1">
                         <img src="${image_url}${image}" onclick="window.location.href='${frontend_base_url}detail.html?id=${id}'" alt="...">
                     </div>
-                    <div class = "btns">
-                        <Button onclick = "Toggle(${id})"class = "btn" id="${id}" style = "color : ${color}" ><i class="fa-solid fa-heart"></i> ${count}</Button>
+                    <div class = "btns" id = "like-button">
+                        <Button onclick = "Toggle(${id})" class = "btn" id="${id}" style = "color : ${color};" ><div class="fa-solid fa-heart" style = "margin-right:10px"></div>${count}</Button>
                     </div>
                     <div class="card-body">
                         <span class="text-uppercase fw-bold fs-6">${title}</span>
                         <p>${content}</p>
+                        <p>댓글 ${count_comment}개</p>
                         <div class="mt-4 about d-flex justify-content-between align-items-center">
                         <a href="${frontend_base_url}detail.html?id=${id}" class="text-dark">더보기...</a>
                         </div>
@@ -68,6 +75,30 @@ async function FeedList(){
 }FeedList()
 }
 Best();
+
+async function Toggle(k) {
+    const btnvar1 = document.getElementById(k);
+    if (btnvar1.style.color == 'red'){
+      btnvar1.style.color = "grey"
+      btnvar1.textContent = Number(btnvar1.textContent) -1
+      btnvar1.innerHTML = `<div class="fa-solid fa-heart" style = "margin-right:10px"></div>${btnvar1.textContent}`;
+    }
+    else {
+      btnvar1.style.color = "red"
+      btnvar1.textContent = Number(btnvar1.textContent) +1
+      btnvar1.innerHTML = `<div class="fa-solid fa-heart" style = "margin-right:10px"></div>${btnvar1.textContent}`;
+
+    }
+
+
+    const response = await fetch(`${backend_base_url}articles/like/${k}/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem("access")
+      },
+    })
+  }
 
 
 
